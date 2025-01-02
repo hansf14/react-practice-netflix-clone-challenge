32fef35198f6dee77f08650984694fa5ef50ac00
hansf14 <sam_h0117@hotmail.com>
2025-01-03 01:03:12 +0900

    svg animation 할 차례

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
-import { motion, Variants } from "motion/react";
-import { useRef } from "react";
+import {
+  motion,
+  useMotionValue,
+  useMotionValueEvent,
+  useScroll,
+  useTransform,
+  Variants,
+} from "motion/react";
+import { useEffect } from "react";
 import { createGlobalStyle, styled } from "styled-components";
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
@@ -74,99 +81,19 @@ const GlobalStyle = createGlobalStyle`
     font-family: 'Source Sans Pro', sans-serif;
     color:black;
     line-height: 1.2;
-    background:linear-gradient(135deg,#e09,#d0e);
   }
 `;
 
-const Wrapper = styled.div`
-  height: 100vh;
+const Wrapper = styled(motion.div)`
+  // height: 100vh;
+  height: 200vh;
   width: 100vw;
   display: flex;
   justify-content: center;
   align-items: center;
+
+  background: linear-gradient(135deg, #e09, #d0e);
 `;
-
-// const Box = styled(motion.div)`
-//   width: 200px;
-//   height: 200px;
-//   background-color: white;
-//   border-radius: 15px;
-//   box-shadow:
-//     0 2px 3px rgba(0, 0, 0, 0.1),
-//     0 10px 20px rgba(0, 0, 0, 0.06);
-// `;
-
-// const Box = styled(motion.div)`
-//   width: 200px;
-//   height: 200px;
-//   display: grid;
-//   grid-template-columns: repeat(2, 1fr);
-//   background-color: rgba(255, 255, 255, 0.2);
-//   border-radius: 40px;
-//   box-shadow:
-//     0 2px 3px rgba(0, 0, 0, 0.1),
-//     0 10px 20px rgba(0, 0, 0, 0.06);
-// `;
-
-// const Circle = styled(motion.div)`
-//   width: 70px;
-//   height: 70px;
-//   place-self: center;
-//   background-color: white;
-//   border-radius: 35px;
-//   box-shadow:
-//     0 2px 3px rgba(0, 0, 0, 0.1),
-//     0 10px 20px rgba(0, 0, 0, 0.06);
-// `;
-
-// const myVariants = {
-//   start: { scale: 0 },
-//   end: { scale: 1, rotateZ: 360, transition: { type: "spring", delay: 0.5 } },
-// };
-
-// const boxVariants: Variants = {
-//   start: { opacity: 0, scale: 0.5 },
-//   end: {
-//     opacity: 1,
-//     scale: 1,
-//     transition: {
-//       type: "spring",
-//       duration: 2,
-//       bounce: 0.5,
-//       delayChildren: 2,
-//       staggerChildren: 0.2,
-//     },
-//   },
-// };
-
-// const circleVariants: Variants = {
-//   start: {
-//     scale: 0,
-//   },
-//   end: {
-//     scale: 2,
-//     transition: {
-//       type: "spring",
-//       bounce: 0.8,
-//       duration: 5,
-//     },
-//   },
-// };
-
-// const circleVariants: Variants = {
-//   start: {
-//     opacity: 0,
-//     y: 10,
-//   },
-//   end: {
-//     opacity: 1,
-//     y: 0,
-//     transition: {
-//       type: "spring",
-//       duration: 3,
-//     },
-//   },
-// };
 
 const BiggerBox = styled.div`
   width: 600px;
@@ -203,35 +130,14 @@ const boxVariants: Variants = {
 };
 
 function App() {
-  const refBiggerBox = useRef<HTMLDivElement | null>(null);
-
   return (
     <>
       <GlobalStyle />
       <Wrapper>
-        {/* <Box
-          variants={boxVariants}
-          whileHover="hover"
-          whileTap="click"
-          whileDrag="drag"
-          drag
-        /> */}
-        <BiggerBox ref={refBiggerBox}>
-          <Box
-            drag
-            // dragConstraints={{
-            //   top: -200,
-            //   left: -200,
-            //   right: 200,
-            //   bottom: 200,
-            // }}
-            dragConstraints={refBiggerBox}
-            // dragSnapToOrigin
-            // dragElastic={1}
-            // dragElastic={0.5}
-            dragElastic={0}
-          />
-        </BiggerBox>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+          <path d="M224 373.1c-25.2-31.7-40.1-59.4-45-83.2-22.6-88 112.6-88 90.1 0-5.5 24.3-20.3 52-45 83.2zm138.2 73.2c-42.1 18.3-83.7-10.9-119.3-50.5 103.9-130.1 46.1-200-18.9-200-54.9 0-85.2 46.5-73.3 100.5 6.9 29.2 25.2 62.4 54.4 99.5-32.5 36.1-60.6 52.7-85.2 54.9-50 7.4-89.1-41.1-71.3-91.1 15.1-39.2 111.7-231.2 115.9-241.6 15.8-30.1 25.6-57.4 59.4-57.4 32.3 0 43.4 25.9 60.4 59.9 36 70.6 89.4 177.5 114.8 239.1 13.2 33.1-1.4 71.3-37 86.6zm47-136.1C280.3 35.9 273.1 32 224 32c-45.5 0-64.9 31.7-84.7 72.8C33.2 317.1 22.9 347.2 22 349.8-3.2 419.1 48.7 480 111.6 480c21.7 0 60.6-6.1 112.4-62.4 58.7 63.8 101.3 62.4 112.4 62.4 62.9 .1 114.9-60.9 89.6-130.2 0-3.9-16.8-38.9-16.8-39.6z" />
+        </svg>
+        ;
       </Wrapper>
     </>
   );
