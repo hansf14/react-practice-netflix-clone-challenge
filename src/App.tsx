356efefc6207e1ac299a5e6cd74c180dc71dc440
hansf14 <sam_h0117@hotmail.com>
2025-01-03 11:15:32 +0900

    Carousel 연습할 예정

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import {
+  AnimatePresence,
   motion,
   useMotionValue,
   useMotionValueEvent,
@@ -6,7 +7,7 @@ import {
   useTransform,
   Variants,
 } from "motion/react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { createGlobalStyle, styled } from "styled-components";
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
@@ -85,59 +86,62 @@ const GlobalStyle = createGlobalStyle`
 `;
 
 const Wrapper = styled(motion.div)`
-  // height: 100vh;
-  height: 200vh;
+  height: 100vh;
   width: 100vw;
   display: flex;
   justify-content: center;
   align-items: center;
-
   background: linear-gradient(135deg, #e09, #d0e);
 `;
 
-const BiggerBox = styled.div`
-  width: 600px;
-  height: 600px;
-  background-color: rgba(255, 255, 255, 0.4);
-  border-radius: 40px;
-
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  // overflow: hidden;
-`;
-
 const Box = styled(motion.div)`
-  width: 200px;
+  width: 400px;
   height: 200px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
+  position: absolute;
+  top: 100px;
   box-shadow:
     0 2px 3px rgba(0, 0, 0, 0.1),
     0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 const boxVariants: Variants = {
-  hover: {
-    scale: 1.5,
-    rotateZ: 90,
+  initial: {
+    opacity: 0,
+    scale: 0,
   },
-  click: {
+  visible: {
+    opacity: 1,
     scale: 1,
-    borderRadius: "100px",
+    rotateZ: 360,
   },
-  drag: { backgroundColor: "rgba(46, 204, 113)", transition: { duration: 10 } },
+  leaving: {
+    opacity: 0,
+    scale: 0,
+    y: 50,
+  },
 };
 
 function App() {
+  const [stateIsShowing, setStateIsShowing] = useState(false);
+  const toggleIsShowing = () => setStateIsShowing((cur) => !cur);
+
   return (
     <>
       <GlobalStyle />
       <Wrapper>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-          <path d="M224 373.1c-25.2-31.7-40.1-59.4-45-83.2-22.6-88 112.6-88 90.1 0-5.5 24.3-20.3 52-45 83.2zm138.2 73.2c-42.1 18.3-83.7-10.9-119.3-50.5 103.9-130.1 46.1-200-18.9-200-54.9 0-85.2 46.5-73.3 100.5 6.9 29.2 25.2 62.4 54.4 99.5-32.5 36.1-60.6 52.7-85.2 54.9-50 7.4-89.1-41.1-71.3-91.1 15.1-39.2 111.7-231.2 115.9-241.6 15.8-30.1 25.6-57.4 59.4-57.4 32.3 0 43.4 25.9 60.4 59.9 36 70.6 89.4 177.5 114.8 239.1 13.2 33.1-1.4 71.3-37 86.6zm47-136.1C280.3 35.9 273.1 32 224 32c-45.5 0-64.9 31.7-84.7 72.8C33.2 317.1 22.9 347.2 22 349.8-3.2 419.1 48.7 480 111.6 480c21.7 0 60.6-6.1 112.4-62.4 58.7 63.8 101.3 62.4 112.4 62.4 62.9 .1 114.9-60.9 89.6-130.2 0-3.9-16.8-38.9-16.8-39.6z" />
-        </svg>
-        ;
+        <button onClick={toggleIsShowing}>Click</button>
+        <AnimatePresence>
+          {stateIsShowing && (
+            <Box
+              variants={boxVariants}
+              initial="initial"
+              animate="visible"
+              exit="leaving"
+            />
+          )}
+        </AnimatePresence>
       </Wrapper>
     </>
   );
